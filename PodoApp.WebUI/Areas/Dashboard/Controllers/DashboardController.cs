f98b5dc70a88d81e177d73afa9c826776c975e08
Jaio Blanco <diablo100_1@hotmail.com>
2018-12-09 15:52:40 +0100

    Add Angle Style and defineAreas

---
+++ PodoApp.WebUI/Areas/Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PodoApp.WebUI.Areas.Dashboard.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard/Dashboard
+        public ActionResult Dashboard_v1()
+        {
+            return View();
+        }
+    }
+}
---
+++ PodoApp.WebUI/Areas/Dashboard/DashboardAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PodoApp.WebUI.Areas.Dashboard
+{
+    public class DashboardAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Dashboard";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Dashboard_default",
+                "Dashboard/{controller}/{action}/{id}",
+                new { action = "Dashboard_v1", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
